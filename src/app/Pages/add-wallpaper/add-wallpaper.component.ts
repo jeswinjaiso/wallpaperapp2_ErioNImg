9ddc0ddbd51342479154c01115ff796c93bc4c0c
Jeswin Jaison <jaisonjeswin63.com>
2025-08-08 19:26:52 +0530

    Commit on 08/08/2025

--- src/app/Pages/add-wallpaper/add-wallpaper.component.ts
+++ src/app/Pages/add-wallpaper/add-wallpaper.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-wallpaper.component.css']
 })
 export class AddWallpaperComponent {
+  base64Image: string | null = null;
+  ConVr: string | null = null;
 
+  onFileSelected(event: Event): void {
+    debugger
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.base64Image = reader.result as string;
+      };
+
+      reader.readAsDataURL(file); 
+    }
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './Pages/home/home.component';
 import { LoginComponent } from './Pages/login/login.component';
+import { AddWallpaperComponent } from './Pages/add-wallpaper/add-wallpaper.component';
 
 
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'add_wallpaper',component:AddWallpaperComponent}
+
 ];
 
 @NgModule({
